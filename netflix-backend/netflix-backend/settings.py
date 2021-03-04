7c0471ca4d2f7def7af67b4bc93027b187731fea
NinaHwang <hwang.ninaa@gmail.com>
2021-03-04 13:26:32 +0900

    [ADD] Movie App

--- netflix-backend/netflix-backend/settings.py
+++ netflix-backend/netflix-backend/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'profileimage',
+    'movie'
 ]
 
 MIDDLEWARE = [
