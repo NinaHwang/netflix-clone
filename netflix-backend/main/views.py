dbe90406a1e1efb4aa76d0a138a32c2f9cde4afe
NinaHwang <hwang.ninaa@gmail.com>
2021-03-10 18:44:55 +0900

    [EDIT] Deleted views.py of main app
        because:
            - need to create a new API and app to provide recommendation

--- netflix-backend/main/views.py
+++ netflix-backend/main/views.py
@@ -1,5 +1,4 @@
-from json
-from jwt
+import json
 
 from django.http import JsonResponse
 from django.views import View
@@ -11,43 +10,3 @@ from movie.models import (
 )
 
 # Create your views here.
-
-class RecommendListView(View):
-    @jwt_utils.token
-    def get(self, request):
-        data = json.loads(request.body)
-
-        # either movie or series
-        category = data['category']
-        tag = data['tag']
-
-        if category == 'movie':
-            targets = Movie.objects.filter(
-                movietag_set_tag=tag
-            )
-            
-            return JsonResponse(
-                {
-                    'movies': [
-                        {
-                            'movie_id': target.id,
-                            'poster': target.poster
-                        } for target in targets
-                    ] 
-                }, status=200
-            )
-
-        targets = Series.objects.filter(
-            seriestag_set_tag=tag
-        )
-
-        return JsonResponse(
-            {
-                'series': [
-                    {
-                        'series_id': target.id,
-                        'poster': target.poster
-                    } for target in targets
-                ]
-            }, status=200
-        )
